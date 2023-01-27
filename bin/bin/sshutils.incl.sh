aa4d8426f38e66d0b22a2849ecd075a0645737f7
dgronskij <gronsky.dm@gmail.com>
2023-01-27 12:31:33 +0300

    refactor(bin)

---
+++ bin/bin/sshutils.incl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# TODO: environment variable to set link location
+
+function _sshutils_relink_ssh_auth() {
+    # https://gist.github.com/martijnvermaat/8070533
+    if [[ -S "$SSH_AUTH_SOCK" && ! -h "$SSH_AUTH_SOCK" ]]; then
+        ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock;
+    fi
+    export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock;
+}
+
+function _sshutils_reset_ssh_auth() {
+    export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock;
+}
