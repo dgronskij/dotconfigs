9c213d41c96b56a6ff480e6e23d97e2cb01c9e5f
dgronskij <gronsky.dm@gmail.com>
2023-09-27 15:08:38 +0300

    feat(nvim): Bdelete command

--- nvim/dgronskiy_nvim/plugins/ui.lua
+++ nvim/dgronskiy_nvim/plugins/ui.lua
@@ -3,5 +3,12 @@ return {
     {
         'rose-pine/neovim',
         name = 'rose-pine',
+    },
+    {
+        "famiu/bufdelete.nvim",
+        cmd = { "Bdelete" },
+        keys = {
+            { "<leader>q", "<cmd>Bdelete<cr>", desc = "Close current buffer (:Bdelete)" }
+        },
     }
 }
