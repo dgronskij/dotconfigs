67fb69b31de4e3184336d47cb225e9940915d332
dgronskij <gronsky.dm@gmail.com>
2024-04-16 14:28:17 +0300

    feat(nvim): neo-tree line numbers

--- nvim/dgronskiy_nvim/plugins/astronvim_overrides.lua
+++ nvim/dgronskiy_nvim/plugins/astronvim_overrides.lua
@@ -10,4 +10,32 @@ return {
         "nvim-treesitter/nvim-treesitter",
         commit = "origin/HEAD", -- astronvim machinery prevents this from updates
     },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = function(plugin, opts)
+            -- vim.deep_tbl_extend does has a side effect of
+            -- trmbling vertical navigation: pressing `k`, e.g.
+            -- can occassionally move cursor up and then back
+            -- I don't why though
+            override_with = {
+                event_handlers = {
+                    { -- ~/.config/astronvim3/lua/plugins/neo-tree.lua
+                        event = "neo_tree_buffer_enter",
+                        handler = function(_)
+                            vim.opt_local.signcolumn = "auto"
+                        end,
+                    },
+                    { -- https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/843
+                        event = "neo_tree_buffer_enter",
+                        handler = function(arg)
+                            vim.opt.relativenumber = true
+                        end,
+                    },
+                },
+            }
+
+            opts.event_handlers = override_with.event_handlers
+            return opts
+        end,
+    },
 }
