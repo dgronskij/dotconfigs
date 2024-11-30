f387caa0d1d5d64e5009a12b67842a67ce9e0f49
dgronskij <gronsky.dm@gmail.com>
2024-11-30 13:55:37 +0300

    feat(nvim): save/load qf

--- nvim/dgronskiy_nvim/astronvim/init.lua
+++ nvim/dgronskiy_nvim/astronvim/init.lua
@@ -391,6 +391,20 @@ local config = {
             end)()
         end
 
+        -- https://www.reddit.com/r/vim/comments/9iwr41/store_quickfix_list_as_a_file_and_load_it/
+        vim.cmd([[
+            function! s:load_file(type, bang, file) abort
+                let l:efm = &l:efm
+                let &l:errorformat = "%-G%f:%l: All of '%#%.depend'%.%#,%f%.%l col %c%. %m"
+                let l:cmd = a:bang ? 'getfile' : 'file'
+                exec a:type.l:cmd.' '.a:file
+                let &l:efm = l:efm
+            endfunction
+
+            command! -complete=file -nargs=1 -bang Cfile call <SID>load_file('c', <bang>0, <f-args>)
+            command! -complete=file -nargs=1 -bang Lfile call <SID>load_file('l', <bang>0, <f-args>)
+            ]])
+
         vim.lsp.set_log_level("info")
     end,
 }
