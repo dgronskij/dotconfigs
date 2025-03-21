e7e7714dc941783c1863ebdef7fb996a06a9047e
dgronskij <gronsky.dm@gmail.com>
2025-03-21 15:30:46 +0300

    feat(nvim): flash.nvim

--- nvim/dgronskiy_nvim/plugins/init.lua
+++ nvim/dgronskiy_nvim/plugins/init.lua
@@ -137,9 +137,24 @@ return {
     {
         "https://github.com/ggandor/leap.nvim",
         event = "VeryLazy",
+        enabled = false,
         config = function()
             require("leap").add_default_mappings()
         end,
+    },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
     },
     {
         -- "https://github.com/niqodea/lasso.nvim",
