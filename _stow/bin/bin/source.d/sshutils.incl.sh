d7537e97ea8ffb64836fa3608eb0a22d4446f2dd
dgronskij <gronsky.dm@gmail.com>
2025-11-07 00:23:34 +0300

    fix(sh/ssh): avoid recursion on SSH_AUTH_SOCK

--- _stow/bin/bin/source.d/sshutils.incl.sh
+++ _stow/bin/bin/source.d/sshutils.incl.sh
@@ -4,18 +4,29 @@
 
 # this function should be called in ~/.profile
 function _sshutils_relink_ssh_auth() {
+
+    local ssh_auth_sock_should_be="$HOME/.ssh/ssh_auth_sock"
+
+    # 1. if relinked by OUR machinery, do nothing
+    #     - is it equivalent to checking SSH_AUTH_SOCK value itself? I think yes
+    if [[ "$SSH_AUTH_SOCK" = "$ssh_auth_sock_should_be ]] ; then
+        return;
+    fi
+
+    # 2. if relinked by NOT US -- relink
+
     # work machine has $SSH_AUTH_SOCK relinked
     if [[ "$(hostname)" = "dgronskiy-osx" ]] ; then
-        ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock;
-        export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock;
+        ln -sf "$SSH_AUTH_SOCK" "$ssh_auth_sock_should_be";
+        export SSH_AUTH_SOCK="$ssh_auth_sock_should_be";
 	return;
     fi
 
     # https://gist.github.com/martijnvermaat/8070533
     if [[ -S "$SSH_AUTH_SOCK" && ! -h "$SSH_AUTH_SOCK" ]]; then
-        ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock;
+        ln -sf "$SSH_AUTH_SOCK" "$ssh_auth_sock_should_be";
     fi
-    export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock;
+    export SSH_AUTH_SOCK="$ssh_auth_sock_should_be";
 }
 
 function _sshutils_reset_ssh_auth() {
